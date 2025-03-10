5e2b4d56a092174cd92275f0989202cddf325fd9
Dmytro Hanenko <hanenkod@roehampton.ac.uk>
2025-03-10 20:29:20 +0000

    sprint 3

--- app/app.js
+++ app/app.js
@@ -1,48 +1,54 @@
-// Import express.js
+// Impot of the required modules
 const express = require("express");
+const path = require("path");
+const db = require('./services/db'); // Database connection
 
-// Create express app
-var app = express();
+// Creating an Express instance
+const app = express();
 
-// Add static files location
-app.use(express.static("static"));
+// Installing PUG as a templating engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views")); // Specify the folder with the templates
 
-// Get the functions in the db.js file to use
-const db = require('./services/db');
 
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
+// Connecting static files (CSS, images)
+app.use(express.static(path.join(__dirname, "../static")));
+app.use(express.static(path.join(__dirname, "images")));
+
+// Route for the main page
+
+app.get("/", (req, res) => {
+  res.render("home-page");
 });
 
-// Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
+app.get("/tutors", async (req, res) => {
+    try {
+      const sql = 'SELECT * FROM Tutors';
+      const tutors = await db.query(sql);
+      res.render("tutor-list", { tutors });
+    } catch (err) {
+      console.error("Error when retrieving data from the database:", err);
+      res.status(500).send("Server error");
+    }
+  });
+
+// Route for testing the database
+app.get("/db_test", async (req, res) => {
+    try {
+        // Example of a query for the table test_table
+        const sql = 'SELECT * FROM test_table';
+        const results = await db.query(sql); // Using asynchronous query
         console.log(results);
-        res.send(results)
-    });
+        res.send(results);
+    } catch (err) {
+        console.error("Database testing error:", err);
+        res.status(500).send("Server error");
+    }
 });
 
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
-    res.send("Goodbye world!");
-});
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
 
-// Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+// Starting the server on port 3000
+app.listen(3000, () => {
+    console.log("The server is running on http://127.0.0.1:3000/");
 });
--- app/services/db.js
+++ app/services/db.js
@@ -2,28 +2,44 @@ require("dotenv").config();
 
 const mysql = require('mysql2/promise');
 
+// Database connection configuration
 const config = {
-  db: { /* do not put password or any sensitive info here, done only for demo */
-    host: process.env.DB_CONTAINER,
-    port: process.env.DB_PORT,
-    user: process.env.MYSQL_ROOT_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    waitForConnections: true,
-    connectionLimit: 2,
-    queueLimit: 0,
-  },
+  host: process.env.DB_CONTAINER,
+  port: process.env.DB_PORT,
+  user: process.env.MYSQL_ROOT_USER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
-  
-const pool = mysql.createPool(config.db);
 
-// Utility function to query the database
+// Create a connection pool
+const pool = mysql.createPool(config);
+
+// Logging a successful connection to the database
+pool.getConnection()
+  .then(connection => {
+    console.log('Successful connection to the database');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('Database connection error:', err);
+    process.exit(1); // Termination of the process in case of connection error
+  });
+
+// Utility function for executing database queries
 async function query(sql, params) {
-  const [rows, fields] = await pool.execute(sql, params);
-
-  return rows;
+  try {
+    const [rows] = await pool.execute(sql, params);
+    return rows;
+  } catch (err) {
+    console.error('Error while executing the query:', err);
+    throw err; // Throw an error for processing in the calling code
+  }
 }
 
+// Export the query function for use in other modules
 module.exports = {
   query,
-}
+};
