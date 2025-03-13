1fcee4ed8440a93cca54761af3612bf41482a1be
Dmytro Hanenko <hanenkod@roehampton.ac.uk>
2025-03-13 19:22:02 +0000

    Sprint 3

--- app/app.js
+++ app/app.js
@@ -32,6 +32,10 @@ app.get("/tutors", async (req, res) => {
     }
   });
 
+  app.get("/profile", (req, res) => {
+    res.render("profile");
+  });
+
 // Route for testing the database
 app.get("/db_test", async (req, res) => {
     try {
